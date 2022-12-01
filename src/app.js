27c94bba7b82a675e7e1d287a8938524e5687ca0
yisas93 <juan249325@gmail.com>
2022-12-01 04:11:02 +0000

    random_card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  generateCard();
   console.log("Hello Rigo from the console!");
 };
+
+function generateCard() {
+  let figures = ["♦", "♥", "♠", "♣"];
+  let numfig = Math.floor(Math.random() * 4);
+  let figure = figures[numfig];
+  let color = numfig <= 1 ? "red" : "black";
+  let num = Math.floor(Math.random() * 12) + 1;
+  switch (num) {
+    case 1:
+      num = "A";
+      break;
+    case 11:
+      num = "J";
+      break;
+    case 12:
+      num = "Q";
+      break;
+    case 13:
+      num = "K";
+      break;
+  }
+  console.log(`figura ${figure} color: ${color} valor: ${num}`);
+  document.getElementById(
+    "card"
+  ).innerHTML = `<div id="arriba">${figure}</div> <div id="num">${num}</div><div id="abajo">${figure}</div>`;
+  document.getElementById("card").style.color = color;
+}
